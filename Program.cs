245e5a8df94c42323020d71d999c3c4d53a6dd89
AntonH <anton160687@gmail.com>
2023-07-07 18:35:47 +0300

    add программа для формирования массива строк

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Program {
+ static void Main() {
+        Console.WriteLine("Введите строки, разделенные запятыми:");
+ string input = Console.ReadLine();
+ string[] words = input.Split(',');
+ string[] newWords = new string[words. Length];
+ int count = 0;
+ for (int i = 0; i < words.Length; i++) {
+ if (words[i].Length <= 3) {
+ newWords[count++] = words[i];
+            }
+        }
+        Array.Resize(ref newWords, count);
+        Console.WriteLine($"Result: {string.Join(",", newWords)}");
+    }
+}
